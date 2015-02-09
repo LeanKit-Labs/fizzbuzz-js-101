1584f4f63f2b94d596616f67a8144a8d6f4e2ace
Chris Gundersen <chrisg@techometry.com>
2015-02-09 15:03:19 -0600

    Clarifying specs

--- spec/fizzbuzz.spec.js
+++ spec/fizzbuzz.spec.js
@@ -14,7 +14,7 @@ describe('fizzbuzz', function () {
      * Specification #1: The function should return the string "fizz"
      * when the function argument is evenly divisible by three.
      */
-    it('should return "fizz" when arg is divisible by three', function () {
+    it('should return "fizz" when the function argument is divisible by three', function () {
         expect(fizzbuzz(3)).toEqual('fizz');
     });
 
@@ -22,7 +22,7 @@ describe('fizzbuzz', function () {
      * Specification #2: The function should return the string "buzz"
      * when the function argument is evenly divisible by five.
      */
-    it('should return "buzz" when arg is divisible by five', function () {
+    it('should return "buzz" when the function argument is divisible by five', function () {
         expect(fizzbuzz(5)).toEqual('buzz');
     });
 
@@ -30,7 +30,7 @@ describe('fizzbuzz', function () {
      * Specification #3: The function should return the string "fizzbuzz"
      * when the function argument is evenly divisible by both three *and* five.
      */
-    it('should return "fizzbuzz" when arg is divisible by both three and five', function () {
+    it('should return "fizzbuzz" when the function argument is divisible by both three and five', function () {
         // You're on your own here. Replace "pending();" with your own code
         pending();
     });
@@ -39,7 +39,7 @@ describe('fizzbuzz', function () {
      * Specification #4: The function should return the string "MeaningOfLife"
      * when the function argument is equal to the number 42.
      */
-    it('should return "MeaningOfLife" when arg is equal to 42', function () {
+    it('should return "MeaningOfLife" when the function argument is equal to 42', function () {
         // You're on your own here. Replace "pending();" with your own code
         pending();
     });
